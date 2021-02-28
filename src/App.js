06f288a85bf14e617931fcb330036d125eabfacf
Syed Wassi Ul Haque <wassi244255@gmail.com>
2021-02-28 18:06:38 +0600

    React Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done 22<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name = "wassi"></Person>
+        <Person name = "abeed"></Person>
+        <Counter></Counter>
+        
       </header>
     </div>
   );
+}
+
+function Person (props) {
+  const style = {
+    border: '2px solid red',
+    margin : '5px'
+  }
+  return (
+  <div style = {style} >
+  <h1> Name :{props.name}</h1>
+  <h1> Name :{props.name}</h1>
+  </div>
+    ) 
+}
+
+function Counter() {
+  const [count, setCount] = useState(0)
+  const handleDecrease = () => {
+    if (count > 0) {
+      setCount (count - 1)
+    }
+    else {
+      console.log("alert");
+    }
+  } ;
+
+  
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  return ( <div>
+    <h1> Count :{count}</h1>
+    <button onClick = {handleIncrease}>Increase</button>
+    <button onClick = {handleDecrease}>Decrease</button>
+
+  </div> )
+
+
 }
 
 export default App;
